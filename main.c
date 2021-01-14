a3e17bc50a9fd6110f21613c07ab912787909059
Guy Kaplan <gkpln3@gmail.com>
2021-01-14 20:27:54 +0200

    Added file read.

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdint.h>
+
+typedef struct {
+    uint32_t magic;
+} elf_header_t;
 
 int main(int argc, char* argv[]) {
+    elf_header_t header;
+
     printf("Hello!\n");
+    
+    FILE* file = fopen("file.out", "rb");
+    fread(&header, sizeof(header), 1, file);
     return 0;
 }
